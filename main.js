ea0cce8036e5f156ab3a38f80f42b7e624e43177
徳永貴文 <proton.ich1@protonmail.com>
2019-08-24 16:16:30 +0900

    [excercise] js-excercise-for-frontend-3
    【エクササイズ】DOMの新規作成・追加・削除・全削除をする【JavaScript】

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
  * 課題1: id属性値が `main` のdiv要素ににul要素を追加する
  */
 
+const main = document.getElementById('main');
+const ulElement = document.createElement('ul');
+main.appendChild(ulElement);
+
 /**
  * 課題2: 課題1で作成したul要素に5つのli要素(DOM)を追加する
  *   - forループを使って5つのli要素(DOM)を追加すること
@@ -13,6 +17,12 @@
  *     5. アイテム5
  */
 
+const count = 5;
+for (let i = 0; i < count; i++) {
+	let liElement = document.createElement('li');
+	liElement.textContent = `アイテム${i + 1}`;
+	ulElement.appendChild(liElement);
+}
 
 /**
  * 課題3: 課題2で作成した5つのli要素の内、先頭から4番目(アイテム4)の要素を削除する
@@ -20,9 +30,13 @@
  *     - https://developer.mozilla.org/ja/docs/Web/API/ParentNode/children
  */
 
+const removeNum = 3;
+ulElement.removeChild(ulElement.children[removeNum]);
 
 /**
  * 課題4: id属性値が `remove-all-items` のul要素内の全てのli要素を削除する
  *   - ul要素は削除しないこと
  */
 
+const removeElement = document.getElementById('remove-all-items');
+while(removeElement.firstChild) removeElement.removeChild(removeElement.firstChild);
